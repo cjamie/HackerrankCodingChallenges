c58f41e3d4c351be14f30da0f95f331e49bf0fb7
Jamie Chu <huanjamierose@gmail.com>
2018-02-23 04:09:26 -0500

    Sock Merchant improvement

--- Implementation/Base2.playground/Contents.swift
+++ Implementation/Base2.playground/Contents.swift
@@ -13,18 +13,12 @@ import UIKit
 //returns ID of the bird
 
 
-let a = 9
+
 let b = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
 var dict = [Int:Int]()
-b.forEach{
-    if let temp = dict[$0]{
-        dict[$0] = temp + 1
-    }else{
-        dict[$0] = 1
-    }
-}
-print(dict)
+b.forEach{ dict[$0] = (dict[$0] ?? 0) + 1 }
 var counter = 0
 dict.forEach{ counter += $0.value/2 }
 print(counter)
+

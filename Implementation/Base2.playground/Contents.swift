ba92c4cae6a77a3a845faa2a40d7155030ace09e
Jamie Chu <huanjamierose@gmail.com>
2018-02-25 12:09:41 -0500

    added import Foundation

--- Implementation/Base2.playground/Contents.swift
+++ Implementation/Base2.playground/Contents.swift
@@ -12,10 +12,30 @@ import UIKit
 
 //let arr = readLine()!.components(separatedBy: " ").map{ String($0)! }
 
-var a = 3
-var b = 1 2 3
-Sample Output 0
+var a = [3, 2, 1]
 
-Array is sorted in 0 swaps.
-First Element: 1
-Last Element: 3
+var counter = 0
+a.forEach{ _ in
+    for j in stride(from: 0, to: a.count-1, by: 1){
+        if a[j] > a[j + 1] {
+            a[j] = a[j] + a[j+1]
+            a[j+1] = a[j] - a[j+1]
+            a[j] = a[j] - a[j+1]
+            counter += 1
+        }
+    }
+}
+
+print("Array is sorted in \(counter) swaps.")
+print("First Element: \(a[0])")
+print("Last Element: \(a[a.count-1])")
+
+//Sample Output 0
+
+//Array is sorted in 0 swaps.
+//First Element: 1
+//Last Element: 3
+
+
+
+

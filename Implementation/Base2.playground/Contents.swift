77e4daf87dd9a0d16e1b1fbce49d49de85da8a65
Jamie Chu <huanjamierose@gmail.com>
2018-02-25 04:18:41 -0500

    added Bit Manipulation: Lonely Integer

--- Implementation/Base2.playground/Contents.swift
+++ Implementation/Base2.playground/Contents.swift
@@ -10,82 +10,11 @@ import UIKit
 // read array and map the elements to integer
 //let arr = readLine()!.components(separatedBy: " ").map{ Int($0)! }
 
-
 //let arr = readLine()!.components(separatedBy: " ").map{ String($0)! }
 
-
-
-
-let a = "{[()]}"
-var b = a.map{$0}
-
-//print(b.popLast())
-
-
-//func isValid(input: [Character])->String{
-//
-//    var stack: [Character] = []
-//    var index = 0
-//
-//    defer{print(stack)}
-//
-//    for char in input{
-//        switch char{
-//        case "{","[","(":
-//            stack.append(char)
-//            index += 1
-//        case ")", "]","}":
-//            if index == 0 || stack[index-1] != char{
-//                return "No" }
-//            index -= 1
-////        case"]":
-////            if index == 0 || stack[index-1] != "["{
-////                print("index2 \(index)");
-////                return "No" }
-////            print("[")
-////            index -= 1
-////        case"}":
-////            if index == 0 || stack[index-1] != "{"{
-////                print("index3 \(index)");
-////                return "No" }
-////            print("{")
-////            index -= 1
-//        default:
-//            return "Invalid"
-//        }
-//    }
-//    return "yes"
-//}
-
-
-func isValid(s:[Character])->Bool {
-    var stack: [Character] = []
-    for c in s{
-        if c == "("{ stack.append(")") }
-        else if c == "{"{ stack.append("}") }
-        else if c == "["{ stack.append("]") }
-        else if stack.isEmpty || stack.popLast() != c{ return false }
-    }
-    return stack.isEmpty
-}
-
-nArr.forEach{ print( isValid(s: $0.map{$0}) ? "YES":"NO") }
-
-
-
-
-
-//stride(from: <#T##Strideable#>, to: <#T##Strideable#>, by: <#T##Comparable & SignedNumeric#>)
-print(isValid(s: b))
-
-//for i in stride(from: 0, to: b.count, by: 1){
-//    switch b[i]{
-//    case "{","[","(":
-//        stack.append(b[i])
-//    case ")","]","}":
-//        stack.popLast()
-//    default:
-//        break
-//    }
-//}
-
+let a = 3
+let b = [1, 1, 2]
+//key is int, and value is occurences
+var dict = [Int:Int]()
+b.map{ dict[$0] = (dict[$0] ?? 0) + 1 }
+print(dict.first{$0.value == 1}!.key)

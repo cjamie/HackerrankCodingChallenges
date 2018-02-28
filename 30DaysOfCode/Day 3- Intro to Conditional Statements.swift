68a6e967b1c520167dbcfb8ab628c44675795630
Jamie Chu <huanjamierose@gmail.com>
2018-02-28 00:49:16 -0500

    day 3 added

---
+++ 30DaysOfCode/Day 3- Intro to Conditional Statements.swift
@@ -0,0 +1,15 @@
+//
+//  Day 3- Intro to Conditional Statements.swift
+//  
+//
+//  Created by Admin on 2/28/18.
+//
+
+import Foundation
+
+// Enter your code here
+let a = Int(readLine()!)!
+
+print( a % 2 == 0 && (a==4 || a > 20) ? "Not Weird" : "Weird")
+
+
